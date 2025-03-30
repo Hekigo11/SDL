5fc965dd9c75db00f660489d5ad47c6625792932
Hekigo11 <jdeguzmansergio@gmail.com>
2025-03-30 11:50:45 +0800

    more changes

--- index.php
+++ index.php
@@ -19,43 +19,6 @@
 
 	<body>
 		
-	<header>
-
-<!-- <div class="custom-dropdown" id="customDropdown"> -->
-<ul class="navbar nav pullDown">
-		<li class="nav-item mx-2">
-			<a class="nav-link " href="#home">Home</a> 
-		</li>
-
-		<li class="nav-item mx-2">
-			<a class="nav-link" href="#services">Services</a>
-		</li>
-		<li class="nav-item mx-2">
-			<a class="nav-link" href="#about">About Us</a>
-		</li>
-		<li class="nav-item mx-2">
-			<a class="nav-link" href="#contact">Contact Us</a>
-		</li>
-	</ul>
-
-<!-- </div> -->
-<a class="navbar-brand" style=" margin-right: 10%;" href="index.php">JIM</a> <!-- Change Reference -->
-
-	<div>
-		<!-- <button class="navbar-toggler" type="button" id="burgerMenu" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button> -->
-		<ul class="navbar nav pullDown">
-		<li class="nav-item mx-2"><a href="" class="nav-link">Deliver</a></li>
-		<li class="nav-item mx-2"><a href="" class="nav-link">Cater</a></li>
-		<li class="nav-item mx-2"><a href="#" class="btn btn-outline-light">Cart</a> <!-- Change Reference --></li>
-		<li class="nav-item mx-2">
-			<a class="nav-link" href="#" data-toggle="modal" data-target="#loginModal">Login</a>
-		</li>
-		</ul>
-	</div>
-
-</header>
-
+		<?php include("modules/navigation.php");?>
 		<?php include("modules/homepage.php"); ?>
 	
---
+++ modules/navigation.php
@@ -0,0 +1,41 @@
+<?php ?>
+
+<header>
+
+<!-- <div class="custom-dropdown" id="customDropdown"> -->
+<ul class="navbar nav pullDown">
+		<li class="nav-item mx-2">
+			<a class="nav-link " href="#home">Home</a> 
+		</li>
+
+		<li class="nav-item mx-2">
+			<a class="nav-link" href="#services">Services</a>
+		</li>
+		<li class="nav-item mx-2">
+			<a class="nav-link" href="#about">About Us</a>
+		</li>
+		<li class="nav-item mx-2">
+			<a class="nav-link" href="#contact">Contact Us</a>
+		</li>
+	</ul>
+
+<!-- </div> -->
+<a class="navbar-brand" style=" margin-right: 3%;" href="index.php">JIM</a> <!-- Change Reference -->
+
+	<div style="display: flex; flex-direction: row;">
+	<!-- <button class="navbar-toggler" type="button" id="burgerMenu" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+	</button> -->
+		<ul class="navbar nav pullDown">
+			<li class="nav-item mx-2"><a href="" class="nav-link">Deliver</a></li>
+			<li class="nav-item mx-2"><a href="" class="nav-link">Cater</a></li> 
+		</ul>
+		<ul class="navbar nav">
+			<li class="nav-item mx-2 no-dropdown"><div><a href="#" class="btn rounded-pill btn-outline-light">My orders</a></div> <!-- Change Reference --></li>
+			<li class="nav-item mx-2 no-dropdown">
+			<a class="btn rounded-pill btn-outline-light" href="#" data-toggle="modal" data-target="#loginModal">Login</a>  
+			</li>
+		</ul>
+	</div>
+
+</header>
