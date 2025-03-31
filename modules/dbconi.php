c79f6562e200e7ac75381681b488836101633b74
JustinCruz4133 <justin.cruz876@adamson.edu.ph>
2025-03-31 15:20:40 +0800

    dbconi

--- modules/dbconi.php
+++ modules/dbconi.php
@@ -1,10 +1,8 @@
 <?php
-//connect to db
-// papaltan pa yung dbname
 $username = "root";
 $password = "";
 $server   = "localhost";
-$dbasename = "cruz_sample";
+$dbasename = "marj";
 
 $dbc = mysqli_connect($server, $username, $password);
 mysqli_select_db($dbc, $dbasename);
