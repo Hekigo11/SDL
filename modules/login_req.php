2ae85d4a1429d815a05330e946b3b0b4c6015c36
JustinCruz4133 <justin.cruz876@adamson.edu.ph>
2025-03-31 13:49:20 +0800

    g

--- modules/login_req.php
+++ modules/login_req.php
@@ -9,7 +9,7 @@ if(trim($_POST['txtusername'])=='' && trim($_POST['txtpassword'])==''){
 if($isallok){
 	include("dbconi.php");
 	// Change yung Column and yung Table name since wala pang DB
-	$query = "SELECT * FROM users WHERE user_id='".mysqli_real_escape_string($dbc, $_POST['txt_idno'])."' and password='".mysqli_real_escape_string($dbc, $_POST['txtpassword'])."'";
+	$query = "SELECT * FROM users WHERE username='".mysqli_real_escape_string($dbc, $_POST['txtusername'])."' and password='".mysqli_real_escape_string($dbc, $_POST['txtpassword'])."'";
 	$result = mysqli_query($dbc, $query);
 	if(mysqli_num_rows($result)>0){ // if with records found
 		$row = mysqli_fetch_array($result);
