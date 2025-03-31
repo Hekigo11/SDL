4c37de827f6277fd986063d8ca84c96db4b900a2
EminImura <emin.imura@adamson.edu.ph>
2025-03-31 20:15:07 +0800

    Verify email and monum

---
+++ marj.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 31, 2025 at 11:39 AM
+-- Server version: 9.1.0
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `marj`
+--
+CREATE DATABASE IF NOT EXISTS `marj` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `marj`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `prod_name` varchar(250) NOT NULL,
+  `prod_price` int NOT NULL,
+  `prod_desc` varchar(2000) NOT NULL,
+  `prod_img` varchar(250) NOT NULL,
+  `prod_cat_id` int NOT NULL,
+  `qty_sold` int NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+CREATE TABLE IF NOT EXISTS `transactions` (
+  `tran_id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int NOT NULL,
+  `amount` int NOT NULL,
+  `date` date NOT NULL,
+  `status_id` int NOT NULL,
+  `address` int NOT NULL,
+  PRIMARY KEY (`tran_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `fname` varchar(250) NOT NULL,
+  `mname` varchar(250) DEFAULT NULL,
+  `lname` varchar(250) NOT NULL,
+  `email_add` varchar(250) NOT NULL,
+  `mobile_num` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `role_id` int NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `fname`, `mname`, `lname`, `email_add`, `mobile_num`, `password`, `role_id`) VALUES
+(1, 'James', '', 'Dayao', 'james.dayao@adamson.edu.ph', '1', '$2y$10$5cHx59VZ2XEPA2Dc3KZRwO7afxBtdryn/h6sAgFsKGAJDmGBoKF5y', 1),
+(2, 'Justin', '', 'Cruz', 'justin.cruz876@adamson.edu.ph', '2', '$2y$10$qkJe1UaqJFzYZT7CX6DnwO7s96rroLk.mFRNHXF8/QL7GomWLJ8dW', 2),
+(3, 'test', '', 'test', 'test', '3', '$2y$10$IA9WMahntUWGuUIz0USCN.oKxsp33i3EY0VCgo.7C2CeD2k29zE/e', 2);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- modules/register_save.php
+++ modules/register_save.php
@@ -1,13 +1,28 @@
 <?php
 $isallok = true;
 $msg = "";
+include("dbconi.php");
+
+$email = mysqli_real_escape_string($dbc, $_POST['txtemail']);
+$mo_num = mysqli_real_escape_string($dbc, $_POST['txtmobilenum']);
+
+$query_check = "SELECT * FROM users WHERE email_add = '$email' OR mobile_num = '$mo_num'";
+$result_check = mysqli_query($dbc, $query_check);
+
+while ($row = mysqli_fetch_assoc($result_check)) {
+    if ($row['email_add'] == $email) {
+        $isallok = false; $msg .= "Email Already Exist!\n";
+    }
+	
+    if ($row['mobile_num'] == $mo_num) {
+        $isallok = false; $msg .= "Mobile Number Already Exist!\n";
+    }
+}
 
 if(trim($_POST['txtfname'])==''){
 	$isallok = false; $msg .="Enter Firstname\n";
 }
-// if(trim($_POST['txtmname'])==''){
-// 	$isallok = false; $msg .="Enter Middlename\n";
-// }
+
 if(trim($_POST['txtlname'])==''){
 	$isallok = false; $msg .="Enter Lastname\n";
 }
@@ -25,7 +40,6 @@ if(trim($_POST['txtpassword'])==''){
 }
 
 if($isallok){
-	include("dbconi.php");
 	$hash_reg = password_hash($_POST['txtpassword'], PASSWORD_DEFAULT);
 	
 	$query = "INSERT INTO users (fname, mname, lname, email_add, mobile_num, password, role_id) VALUES (
