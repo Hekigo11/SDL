5e7d20bd20b0ae7956c38bbb2de2892e5f845a57
Ugot-Aaron-Paul <aaron.paul.ugot@adamson.edu.ph>
2025-04-23 11:22:38 +0800

    index title

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ require_once 'config.php';
         <!-- Font Awesome For Icons -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 		<link rel="stylesheet" href="vendor/style1.css">
-		<title>Deliver</title>
+		<title>MARJ Food Services</title>
 
         <style>
             .service-card {
