b9e212fe626c43009db550092405818985935833
Hekigo11 <jdeguzmansergio@gmail.com>
2025-05-20 20:23:47 +0800

    Remove Stripe secret from code, use .env for secrets

--- config/paymongo.php
+++ config/paymongo.php
@@ -1,2 +1,5 @@
 <?php
-define('PAYMONGO_SECRET_KEY', 'sk_test_thjRythHdEBKPzAqBVPJvKGE');
+// Load Stripe secret key from environment variable
+// Make sure to use a package like vlucas/phpdotenv if you want to support .env files directly, or rely on getenv if your environment loads them
+$paymongo_secret_key = getenv('STRIPE_TEST_KEY');
+define('PAYMONGO_SECRET_KEY', $paymongo_secret_key);
