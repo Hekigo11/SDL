7a285f452e541cca9a2a21b7425fc1acd88e39b3
Hekigo11 <jdeguzmansergio@gmail.com>
2025-05-20 20:24:51 +0800

    test

--- config/paymongo.php
+++ config/paymongo.php
@@ -1,5 +1,5 @@
 <?php
-// Load Stripe secret key from environment variable
-// Make sure to use a package like vlucas/phpdotenv if you want to support .env files directly, or rely on getenv if your environment loads them
+// Load Stripe secret key from environment variable only
+// Do NOT put any secret key directly in this file!
 $paymongo_secret_key = getenv('STRIPE_TEST_KEY');
 define('PAYMONGO_SECRET_KEY', $paymongo_secret_key);
